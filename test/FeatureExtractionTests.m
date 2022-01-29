90117539a013dbce63efb6e0687f0dbb9ee3c835
Trevor Vannoy <trevorvannoy@montana.edu>
2022-01-29 12:16:34 -0700

    Update unit tests after replacing findpeaks
    - add test to make sure the fundamental frequency estimation matches the
      original
    - use relative tolerance of 1%; my findpeaks implementation returns
      ever-so-slightly different values for some of the peak features.

--- test/FeatureExtractionTests.m
+++ test/FeatureExtractionTests.m
@@ -37,7 +37,27 @@ classdef FeatureExtractionTests < matlab.unittest.TestCase
 
             newFeatures = extractFeatures(testCase.data(1:1000,:));
 
-            testCase.verifyEqual(newFeatures, table2array(ogFeatures));
+            testCase.verifyEqual(newFeatures, table2array(ogFeatures),...
+                "RelTol", cast(1e-2, 'like', newFeatures));
+        end
+        function testFundamentalExtractionAgainstOriginal(testCase)
+            % Make sure my codegen-ready version does the same thing
+            % as the original code
+
+            esd = abs(fft(testCase.data(1:1000,:)).^2);
+            esd = esd(:,1:end/2);
+            esd = esd./esd(:,1);
+
+            p = path;
+            addpath('../original-code')
+
+            ogFundamentals = estimateFundamentalFreq(esd);
+
+            path(p);
+
+            newFundamentals = estimateFundamentalFreq(esd);
+
+            testCase.verifyEqual(newFundamentals, ogFundamentals);
         end
     end
 end
