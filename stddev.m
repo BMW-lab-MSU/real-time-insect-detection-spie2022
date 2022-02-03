a2fa7fd96eb0847f9b1fad4f73c89f622dafa892
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-03 12:56:05 -0700

    Split std dev computation into parts
    Doing this should hopefully help me control bit growth and pipelining
    easier

--- stddev.m
+++ stddev.m
@@ -1,3 +1,9 @@
 function s = stddev(x, avg)
 
-s = sqrt(sum(abs(x - mean(x,2)).^2, 2) ./ (size(x,2)-1));
+normalization = size(x,2) - 1;
+
+partial1 = abs(x - avg).^2;
+partial2 = sum(partial1, 2);
+partial3 = partial2 / normalization;
+s = sqrt(partial3);
+% s = sqrt(sum(abs(x - mean(x,2)).^2, 2) ./ (size(x,2)-1));
