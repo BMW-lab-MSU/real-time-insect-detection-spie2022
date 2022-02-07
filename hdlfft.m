908ee75565f7198eecb3f1a331ee45b0b4c19535
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-06 18:43:42 -0700

    HDL-compatibility fixes
    1. for some reason, "constant" variables were not being interpreted as
      static, so HDL coder was complaining about variable-size arrays
        - used hardcoded literals to fix the issue
    2. Remove unsupported mod function
    
    Annoyingly, the MATLAB example I based this code off of is obviously not
    HDL compatible, otherwise I wouldn't have needed to make these changes.
    Why in the world would an example of the HDL optimized FFT not be HDL
    compatible...?

--- hdlfft.m
+++ hdlfft.m
@@ -24,33 +24,44 @@ persistent ft;
 if isempty(ft)
     ft = dsp.HDLFFT('FFTLength', 1024, 'BitReversedOutput', false);
 end
-persistent loopCount
-if isempty(loopCount)
-    % Number of iterations needed to get the all the data through
-    % the fft while accounting for the latency of the fft.
-    loopCount = getLatency(ft, 1024, 64) + 16;
-end
+% persistent loopCount
+% if isempty(loopCount)
+%     % Number of iterations needed to get the all the data through
+%     % the fft while accounting for the latency of the fft.
+%     loopCount = getLatency(ft, 1024, 64) + 16;
+%     % loop count = 131
+% end
 
 % HDLFFT outputs the same data type as the input, so we need to
 % convert the input to complex numbers to get complex output.
 xComplex = complex(x);
 
+y = complex(zeros(size(x), 'like', x));
+
 % Split the input into vectors of length 64 for processing in
 % the HDLFFT system object
-xVect = reshape(xComplex, VECTOR_LEN, N_CHUNKS);
+xVect = reshape(xComplex, 64, 16);
 
-yVect = complex(zeros(VECTOR_LEN, loopCount, 'like', x));
-validOut = false(VECTOR_LEN, loopCount);
+yVect = complex(zeros(64, 131, 'like', x));
+validOut = false(131, 1);
+
+yValid = complex(zeros(size(xVect), 'like', xVect));
+
 
 i = cast(0, 'uint8');
 
-for loop = 1:loopCount
+for loop = 1:131
     % Select which 64-length portion of the signal to feed into the fft
-    if mod(loop, N_CHUNKS) == 0
-        i(:) = N_CHUNKS;
+    if loop >= 16
+        i(:) = 16;
     else
-        i(:) = mod(loop, N_CHUNKS);
+        i(:) = loop;
     end
+    % if mod(loop, N_CHUNKS) == 0
+    %     i(:) = N_CHUNKS;
+    % else
+    %     i(:) = mod(loop, N_CHUNKS);
+    % end
 
     % Input data is only valid until we reach the last 64-length
     % portion of the input signal. After that, the input data is 
@@ -61,5 +72,5 @@ for loop = 1:loopCount
 end
 
 % Grab the valid output data and flatten it back into a column vector
-yValid = yVect(:,validOut==1);
-y = yValid(:);
+yValid(:) = yVect(:,validOut==1);
+y(:) = yValid(:);
