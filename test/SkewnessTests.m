dbdec8745bee0b0ba970dc62b8e48b80a774fc48
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-06 18:48:05 -0700

    Test skewness on doubles and singles

--- test/SkewnessTests.m
+++ test/SkewnessTests.m
@@ -9,7 +9,7 @@ classdef SkewnessTests < matlab.unittest.TestCase
     end
 
     methods (Test)
-        function testAgainstMatlabImplementation(testCase)
+        function testAgainstMatlabImplementationDoubles(testCase)
             % Make sure my codegen-ready version does the same thing
             % as the original code
 
@@ -22,7 +22,22 @@ classdef SkewnessTests < matlab.unittest.TestCase
             % answers. Being off by a few times the machine precision
             % is no big deal.
             testCase.verifyEqual(result, expected, ...
-                "RelTol", 3*eps(class(result)));
+                "AbsTol", eps(class(result)));
+        end
+        function testAgainstMatlabImplementationSingles(testCase)
+            % Make sure my codegen-ready version does the same thing
+            % as the original code
+
+            data = rand(10000, 500, 'single');
+            expected = skewness(data, 1, 2);
+            result = codegenSkewness(data);
+
+            % The very large majority of the results are within
+            % 2*eps (twice the precision of doubles) of the expected
+            % answers. Being off by a few times the machine precision
+            % is no big deal.
+            testCase.verifyEqual(result, expected, ...
+                "AbsTol", eps(class(result)));
         end
     end
 end
