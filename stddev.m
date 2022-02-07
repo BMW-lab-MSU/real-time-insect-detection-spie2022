e6d6220c89fb00406ff2628b56a0eeefbd06fc22
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-06 18:47:40 -0700

    replace unsupported abs function

--- stddev.m
+++ stddev.m
@@ -4,8 +4,9 @@ function s = stddev(x, avg)
 
 normalization = size(x,2) - 1;
 
-partial1 = abs(x - avg).^2;
-partial2 = sum(partial1, 2);
-partial3 = partial2 / normalization;
-s = sqrt(partial3);
+partial1 = (x - avg);
+partial2 = real(partial1).^2 + imag(partial1).^2;
+partial3 = sum(partial2, 2);
+partial4 = partial3 / normalization;
+s = sqrt(partial4);
 % s = sqrt(sum(abs(x - mean(x,2)).^2, 2) ./ (size(x,2)-1));
