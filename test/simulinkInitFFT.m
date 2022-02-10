53f2f92cf65c6d3ac2681e54f1d8444179483e04
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-10 08:50:14 -0700

    Add more parameters to make the FFT model more generic
    There are still a few hardcoded constants in the MATLAB function blocks
    because the MATLAB function blocks can't use struct fields during code
    generation. To get around this, I could save those particular parameters
    as normal variables in the workspace.

--- test/simulinkInitFFT.m
+++ test/simulinkInitFFT.m
@@ -5,4 +5,7 @@ testData = single([1:1024; 2*(1:1024); 3*(1:1024); 4*(1:1024)]);
 params.systemClockPeriod = 25e-9;
 params.samplingPeriod = 781.25e-6;
 
+params.FFTSize = 1024;
+params.rowLength = 1024;
+
 stopTime = 5*params.samplingPeriod;
