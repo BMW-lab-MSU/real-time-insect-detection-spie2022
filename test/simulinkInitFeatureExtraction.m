727f86bd0a79502609240f086285a367a3c51396
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-12 19:10:10 -0700

    Combine subsystems into full feature extraction model
    To deal with the different latencies of each subsystem, I had to insert
    some enabled registers that were enabled by validOut signals.

---
+++ test/simulinkInitFeatureExtraction.m
@@ -0,0 +1,18 @@
+% testData = fi([1:1024; 2*(1:1024); 3*(1:1024); 4*(1:1024)]);
+% testData = single([1:1024; 2*(1:1024); 3*(1:1024); 4*(1:1024)]);
+testData = rand(2,1024, 'single');
+% testData = fi(rand(2,1024), 1, 20, 16);
+
+% t = 1:1024;
+% testData = single([sin(2*pi*50*t/1024) + sin(2*pi*150*t/1024) + sin(2*pi*250*t/1024);...
+%     sin(2*pi*100*t/1024) + sin(2*pi*200*t/1024) + sin(2*pi*300*t/1024)]) + 0.5;
+
+
+
+params.systemClockPeriod = 25e-9;
+params.samplingPeriod = 781.25e-6;
+
+params.FFTSize = 1024;
+params.rowLength = 1024;
+
+stopTime = 3*params.samplingPeriod;
