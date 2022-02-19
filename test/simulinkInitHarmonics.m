4a2eb13683d270db9abcee0f173eb3ab6bbb2d72
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-19 12:34:25 -0700

    Fixed point conversion and HDL generation updates
    The new fixed point data types result in little error compared to the
    expected floating-point results. Unfortunately, there's no way this
    design will fit in the FPGA. The findpeaks function alone uses 2.8
    million adders... Since it is a MATLAB function block, there is not much
    optimization that can be done; plus, the serial algorithm just plain
    doesn't map well to hardware resources...

--- test/simulinkInitHarmonics.m
+++ test/simulinkInitHarmonics.m
@@ -7,7 +7,12 @@ testData = rand(2,1024, 'single');
 % testData = single([sin(2*pi*50*t/1024) + sin(2*pi*150*t/1024) + sin(2*pi*250*t/1024);...
 %     sin(2*pi*100*t/1024) + sin(2*pi*200*t/1024) + sin(2*pi*300*t/1024)]) + 0.5;
 
+spectrum = fft(testData, [], 2);
+esd = real(spectrum).^2 + imag(spectrum).^2;
+esd = esd(:,1:end/2);
+esd = esd ./ esd(:,1);
 
+esd = fi(esd, 0, 27, 26);
 
 params.systemClockPeriod = 25e-9;
 params.samplingPeriod = 781.25e-6;
@@ -15,4 +20,4 @@ params.samplingPeriod = 781.25e-6;
 params.FFTSize = 1024;
 params.rowLength = 1024;
 
-stopTime = 3*params.samplingPeriod;
+stopTime = (height(esd))*params.samplingPeriod;
