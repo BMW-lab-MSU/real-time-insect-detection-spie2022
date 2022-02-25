4a0c531827decb19f963048f28f733f9b6640679
Trevor Vannoy <trevorvannoy@montana.edu>
2022-02-24 17:30:42 -0700

    Don't need to set up Quartus path anymore
    Seems like MATLAB is detecting Quartus via environment variables now

--- pathsetup.m
+++
@@ -1,4 +0,0 @@
-QUARTUS_ROOTDIR = getenv('QUARTUS_ROOTDIR');
-quartus_bin = [QUARTUS_ROOTDIR filesep 'bin' filesep 'quartus'];
-
-hdlsetuptoolpath('ToolName', 'Altera Quartus II', 'ToolPath', quartus_bin);
